58e2a079563863a60500cce67cceeacf41709c4b
Rohit Bisht <rohitbisht75890@gmail.com>
2026-07-17 23:00:48 +0530

    Function basics questions

---
+++ function basic questions/helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int great()
+{
+   cout<<"Hello world"<<endl;
+   return 0;
+}
+int main()
+{
+    great();
+
+}
---
+++ function basic questions/sumoftwono.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void sum()
+{
+
+    int num1,num2;
+    cout<<"Enter Num1: "<<endl;
+    cin>>num1;
+    cout<<"Enter Num2: "<<endl;
+    cin>>num2;
+    int c = num1 +num2;
+    cout << "The Sum is " << c;
+
+}
+int main()
+{
+   sum();
+
+}
